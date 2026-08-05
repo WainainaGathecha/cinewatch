340f5cee9dceff62805ea4709c8751f7360d13d6
Wainaina Gathecha <wainainagathecha@gmail.com>
2026-08-05 12:19:14 +0300

    feat: implement HomePage

---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import HeroSection from "@/components/ui/movies/HeroSection";
+import PopularSection from "@/components/ui/movies/PopularSection";
+import TrendingSection from "@/components/ui/movies/TrendingSection";
+
+export default function HomePage() {
+    return (
+        <>
+            <HeroSection />
+            <PopularSection />
+            <TrendingSection />
+        </>
+    )
+}
